5ffe0f2a0ec7b697da476296fb1d690ae8c4d3ef
harmartim-oss <harmartim@gmail.com>
2025-08-10 01:01:13 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/TH_Site/'
+  base: '/TH_Site/',
+  build: {
+    // Explicitly disable Server-Side Rendering
+    ssr: false, 
+  }
 })
